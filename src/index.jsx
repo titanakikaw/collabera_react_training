972603fd30480b82a93a9746602a043ff865771d
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-01-31 07:33:47 +0530

    commit code for initial component

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import '../style.scss';
 
 const container = document.getElementById('root');
 
 const root = createRoot(container);
 
-root.render(<h1>Hello Yagnesh</h1>);
+// Reusable Component
+
+const bgColor = 'red';
+const color = 'white';
+
+// First letter of the component name should be capital
+// From Each component return single Element
+// inline style should be object and property name should be in camel case
+// instead of class use className
+
+// props are immutable
+function App({ title, desc }) {
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <h2>{desc}</h2>
+      <input type="checkbox" />
+    </div>
+  );
+}
+
+root.render(
+  <>
+    <App title="Yagnesh Modh" desc="Full Stack Developer" />
+    <App title="Virat Kohli" desc="Cricketor" />
+    <App title="Rohit Sharma" desc="Cricketor" />
+  </>,
+);
